9a107d9eedaadfc3544cc8e59598b8c0d59a936a
obafemi oderanti <thevetdoctor@gmail.com>
2025-09-28 00:58:07 +0100

    update tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+   server: {
+    port: 5173, // matches Cypress baseUrl
+  },
   plugins: [react()],
 });
